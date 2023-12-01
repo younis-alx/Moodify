15537e104862a9be8cc7f09a2f4db46175299f53
Younis <younisalx-swe@hotmail.com>
2023-12-01 18:17:32 +0300

    Add method to append API key to the list

--- twitter_api/APIKeyManager.py
+++ twitter_api/APIKeyManager.py
@@ -22,6 +22,26 @@ class APIKeyManager:
     def reset_api_keys(self):
         """Resets the API keys to the first one in the list"""""
         self.api_keys = itertools.cycle(os.getenv('X-API-KEY').split(','))
+    
+    def append_api_key(self, file_path, new_value, env_var='X-API-KEY'):
+        """Appends the given API key to the list of API keys"""""
+        
+        # Check if the API key is already in the list
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+
+        found_x_api_key = False
+        for line in lines:
+            if line.strip().startswith(f'{env_var}'):
+                found_x_api_key = True
+                parts = line.strip().split('=')
+                new_value = parts[1] + '=' + new_value
+                new_line = f'{parts[0]}={new_value}'
+                lines[lines.index(line)] = new_line #TODO: Fix this
+                break
+
+        with open(file_path, 'w') as f:
+            f.writelines(lines)
 
 
 
@@ -39,5 +59,7 @@ if __name__ == '__main__':
     api_manager.reset_api_keys # REST TO API_KEY_1
     API_KEY_1 = api_manager.get_next_api_key()
     print(API_KEY_1, len(API_KEY_1)) # API_KEY_1
+    api_manager.append_api_key('.env', 'bs', 'test')
+    print(os.getenv('test').split(','))
 
         
