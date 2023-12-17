86b0dd77fcc32518c18025cbd67634bf78b5fd20
Younis <younisalx-swe@hotmail.com>
2023-12-17 17:06:32 +0300

    Add debug mode to app.run()

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -31,4 +31,4 @@ Swagger(app)
 
 
 if __name__ == '__main__':
-    asyncio.run(app.run(host='0.0.0.0', port=5000, threaded=True))
+    asyncio.run(app.run(host='0.0.0.0', port=5000, threaded=True, debug=True))
