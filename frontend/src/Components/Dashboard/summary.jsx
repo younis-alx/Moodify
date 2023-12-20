a04bc216595242dc5c0b0a15d708b09e907bbb56
Younis <younisalx-swe@hotmail.com>
2023-12-20 14:33:11 +0300

    Add SummaryTable component to Dashboard

---
+++ frontend/src/Components/Dashboard/summary.jsx
@@ -0,0 +1,121 @@
+import { InformationCircleIcon } from "@heroicons/react/solid";
+import {
+  BadgeDelta,
+  Flex,
+  Icon,
+  MultiSelect,
+  MultiSelectItem,
+  Select,
+  SelectItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  Title,
+} from "@tremor/react";
+import { useState } from "react";
+
+import { v4 as uuid4 } from "uuid";
+
+const deltaTypes = {
+  Neutral: "unchanged",
+  Positive: "moderateIncrease",
+  Negative: "moderateDecrease",
+};
+
+export default function SummaryTable({ combined_replies }) {
+  const [selectedStatus, setSelectedStatus] = useState("all");
+  const [selectedNames, setSelectedNames] = useState([]);
+  const relavantData = combined_replies.map(item => {
+      return {
+          name: item.user_replies.user_name,
+          id : uuid4(),
+          tweet_id: item.tweet_id,
+          Postive: item.sentiment.flat().map((item) => item.label === 'Positive' ? item.score : 0).reduce((a, b) => a + b, 0) * 100,
+          Negative: item.sentiment.flat().map((item) => item.label === 'Negative' ? item.score : 0).reduce((a, b) => a + b, 0) * 100,
+          Neutral: item.sentiment.flat().map((item) => item.label === 'Neutral' ? item.score : 0).reduce((a, b) => a + b, 0) * 100,
+          date: item.created_at,
+          text: item.reply_text,
+          reply_to_tweet: item.reply_to_tweet_id,
+          location: item.user_replies.location || 'unknown',
+          status: item.sentiment.flat().reduce((prev, current) => (prev.score >= current.score) ? prev : current).label
+        }
+    })
+    console.log(JSON.stringify(relavantData, null, 2));
+  const isSalesPersonSelected = (salesPerson) =>
+    (salesPerson.status === selectedStatus || selectedStatus === "all") &&
+    (selectedNames.includes(salesPerson.name) || selectedNames.length === 0);
+
+  return (
+    <>
+      <div>
+        <Flex className="space-x-0.5" justifyContent="start" alignItems="center">
+          <Title> Summary Table </Title>
+          <Icon
+            icon={InformationCircleIcon}
+            variant="simple"
+            tooltip="Summary Table"
+          />
+        </Flex>
+      </div>
+      <div className="flex space-x-2">
+        {/* <MultiSelect
+          className="max-w-full sm:max-w-xs"
+          onValueChange={setSelectedNames}
+          placeholder="Select name..."
+        >
+          {relavantData.map((item) => (
+            <MultiSelectItem key={item.id} value={item.name}>
+              {item.name}
+            </MultiSelectItem>
+          ))}
+        </MultiSelect> */}
+        {/* <Select
+          className="max-w-full sm:max-w-xs"
+          defaultValue="all"
+          onValueChange={setSelectedStatus}
+        >
+          <SelectItem value="all">All Performances</SelectItem>
+          <SelectItem value="Positive">Overperforming</SelectItem>
+          <SelectItem value="Neutral">Average</SelectItem>
+          <SelectItem value="Negative">Underperforming</SelectItem>
+        </Select> */}
+      </div>
+        <div className="max-h-96 overflow-y-auto">
+
+            <Table className="mt-6">
+                <TableHead>
+                <TableRow className="mt-6">
+                    <TableHeaderCell>Name</TableHeaderCell>
+                    <TableHeaderCell className="text-right">Location</TableHeaderCell>
+                    <TableHeaderCell className="text-right">Status</TableHeaderCell>
+                </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    
+                {relavantData
+                    .filter((item) => isSalesPersonSelected(item))
+                    .map((item) => (
+                        <TableRow key={item.id} className="">
+                        <TableCell>{item.name}</TableCell>
+                        {/* <TableCell className="text-right">{parseFloat(item.Postive).toFixed(2)}</TableCell>
+                        <TableCell className="text-right">{parseFloat(item.Negative).toFixed(2)}</TableCell>
+                        <TableCell className="text-right">{parseFloat(item.Neutral).toFixed(2)}</TableCell> */}
+                        <TableCell className="text-right">{item.location}</TableCell>
+                        {/* <TableCell className="text-right whitespace-normal">{item.text}</TableCell> */}
+                        <TableCell className="text-right">
+                        <BadgeDelta deltaType={deltaTypes[item.status]} size="xs">
+                            {item.status}
+                        </BadgeDelta>
+                        </TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    </>
+  );
+}
